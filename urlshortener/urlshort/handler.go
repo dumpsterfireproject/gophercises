0dc3bebb4129198ac034137a95b0e52e850fbaf1
Brian Hnat <brian.hnat@gmail.com>
2022-02-13 07:22:41 -0500

    exercise 2

---
+++ urlshortener/urlshort/handler.go
@@ -0,0 +1,68 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+type mapEntry struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		path := request.URL.Path
+		if redirect, ok := pathsToUrls[path]; ok {
+			http.Redirect(writer, request, redirect, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(writer, request)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yml []byte) ([]mapEntry, error) {
+	var entries []mapEntry
+	err := yaml.Unmarshal(yml, &entries)
+	return entries, err
+}
+
+func buildMap(entries []mapEntry) map[string]string {
+	paths := make(map[string]string)
+	for _, entry := range entries {
+		paths[entry.Path] = entry.Url
+	}
+	return paths
+}
